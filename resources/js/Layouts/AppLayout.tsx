f2f2a2682aa84566ac4817c96a2cf21662b9458e
Sujan Moi <sujan.viacon@gmail.com>
2023-09-22 18:07:53 +0530

    added

--- resources/js/Layouts/AppLayout.tsx
+++ resources/js/Layouts/AppLayout.tsx
@@ -38,6 +38,14 @@ const AppLayout = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivE
                 </div>
             </div>
 
+            <div className='my-5 alert'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                </svg>
+                <span>Created With React Js and Type Script</span>
+            </div>
+
             <main>{props.children}</main>
         </div >
     })
